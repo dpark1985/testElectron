4a9150c3b094a1b741cd74dbbd71d3293e8804b6
Daniel Park <dpark1985@gmail.com>
2018-07-03 13:38:18 +0900

    a

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -21,6 +21,9 @@ function createWindow() {
 		// when you should delete the corresponding element.
 		win = null
 	})
+
+	const mainMenu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(mainMenu);
 }
 
 // This method will be called when Electron has finished
@@ -45,5 +48,92 @@ app.on('activate', () => {
 	}
 })
 
-  // In this file you can include the rest of your app's specific main process
-  // code. You can also put them in separate files and require them here.
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+
+
+const menuTemplate = [
+	{
+		label: 'Edit',
+		submenu: [
+			{ role: 'undo' },
+			{ role: 'redo' },
+			{ type: 'separator' },
+			{ role: 'cut' },
+			{ role: 'copy' },
+			{ role: 'paste' },
+			{ role: 'pasteandmatchstyle' },
+			{ role: 'delete' },
+			{ role: 'selectall' }
+		]
+	},
+	{
+		label: 'View',
+		submenu: [
+			{ role: 'reload' },
+			{ role: 'forcereload' },
+			{ role: 'toggledevtools' },
+			{ type: 'separator' },
+			{ role: 'resetzoom' },
+			{ role: 'zoomin' },
+			{ role: 'zoomout' },
+			{ type: 'separator' },
+			{ role: 'togglefullscreen' }
+		]
+	},
+	{
+		role: 'window',
+		submenu: [
+			{ role: 'minimize' },
+			{ role: 'close' }
+		]
+	},
+	{
+		role: 'help',
+		submenu: [
+			{
+				label: 'Learn More',
+				click() { require('electron').shell.openExternal('https://electronjs.org') }
+			}
+		]
+	}
+]
+
+if (process.platform === 'darwin') {
+	menuTemplate.unshift({
+		label: app.getName(),
+		submenu: [
+			{ role: 'about' },
+			{ type: 'separator' },
+			{ role: 'services', submenu: [] },
+			{ type: 'separator' },
+			{ role: 'hide' },
+			{ role: 'hideothers' },
+			{ role: 'unhide' },
+			{ type: 'separator' },
+			{ role: 'quit' }
+		]
+	})
+
+	// Edit menu
+	menuTemplate[1].submenu.push(
+		{ type: 'separator' },
+		{
+			label: 'Speech',
+			submenu: [
+				{ role: 'startspeaking' },
+				{ role: 'stopspeaking' }
+			]
+		}
+	)
+
+	// Window menu
+	menuTemplate[3].submenu = [
+		{ role: 'close' },
+		{ role: 'minimize' },
+		{ role: 'zoom' },
+		{ type: 'separator' },
+		{ role: 'front' }
+	]
+}
